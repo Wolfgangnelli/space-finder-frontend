2e1d3ee9f13ac459dc8c29cd110ff50b06224e0b
[AndreaS] <solfa_90@hotmail.it>
2021-08-04 11:29:14 +0200

    add class components, authservice, user model

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../index.css';
+import { User } from '../models/user.model';
+import { AuthService } from '../services/AuthService';
 
-function App() {
-  return (
-    <div></div>
-  );
+interface AppState {
+  user: User | undefined
 }
 
-export default App;
+export class App extends React.Component<{}, AppState> {
+
+  private authService: AuthService = new AuthService();
+
+  render() {
+    return <div>App works</div>;
+  }
+}
+
+
+
